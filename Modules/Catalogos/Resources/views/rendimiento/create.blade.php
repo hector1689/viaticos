e2cdda51c2c8f346412db9e5930d2d2b6622595a
hector1689 <hector_azul105@hotmail.com>
2022-04-26 10:28:08 -0500

    label rendimiento

--- Modules/Catalogos/Resources/views/rendimiento/create.blade.php
+++ Modules/Catalogos/Resources/views/rendimiento/create.blade.php
@@ -4,7 +4,7 @@
 <div class="card card-custom example example-compact">
 <div class="card-header">
 <h3 class="card-title">
-   Nuevo Peaje
+   Nuevo Rendimiento
 </h3>
 <div class="card-toolbar">
 <div class="example-tools justify-content-center">
